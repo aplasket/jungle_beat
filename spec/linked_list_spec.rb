ba06e0cff88c0e90249b9b69f80dabd47b698012
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-25 19:06:54 -0400

    Write test for includes? method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -71,4 +71,16 @@ describe LinkedList do
     expect(list.find(2,1)).to eq("shi")
     expect(list.find(1,3)).to eq("woo shi shu")
   end
+
+  it "test_includes_method" do
+    list = LinkedList.new
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.includes?("deep")).to be true
+    expect(list.includes?("dep")).to be false
+  end
 end
